847e08371e695ca046d6263e53b7a8e49163f0e4
WkhongorzulW <bkhongorzul064@gmail.com>
2023-02-01 19:11:48 +0800

    ...

--- fe/src/components/ProductTable.jsx
+++ fe/src/components/ProductTable.jsx
@@ -1,14 +1,27 @@
 import { Container, Typography } from "@mui/material";
 import { Box } from "@mui/system";
+import { useEffect } from "react";
 
-export default function ProductTable({ users }) {
+export default function ProductTable({ products, setProducts }) {
+  const URL = "http://localhost:8080/products";
+
+  useEffect(() => {
+    fetchUserData();
+    console.log(products);
+  }, []);
+
+  async function fetchUserData() {
+    const FETCHED_DATA = await fetch(URL);
+    const FETCHED_JSON = await FETCHED_DATA.json();
+    setProducts(FETCHED_JSON.data);
+  }
   return (
     <Container>
-      {users &&
-        users.map((user, index) => {
+      {products &&
+        products.map((product, index) => {
           return (
             <Container>
-              <Typography variant="h5">{user.productname}</Typography>
+              <Typography variant="h5">{product.productname}</Typography>
             </Container>
           );
         })}
