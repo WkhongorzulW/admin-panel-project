99411e0ec5b5694345bb65afec0f1e215744db18
WkhongorzulW <bkhongorzul064@gmail.com>
2023-02-01 18:45:38 +0800

    nodemon crashed ???

--- fe/src/components/UserForm.jsx
+++ fe/src/components/UserForm.jsx
@@ -16,74 +16,79 @@ export default function UserForm() {
         ADD USER
       </Typography>
       <Box maxWidth="md" sx={{ margin: "0 auto" }}>
-        <FormControl
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            gap: 2,
-            flexWrap: "wrap",
-            justifyContent: "center",
-          }}
-          fullWidth={true}
-          onSubmit
-        >
-          <TextField
-            type={"text"}
-            label={"First name"}
-            variant={"outlined"}
-            fullWidth={"true"}
-          />
-          <TextField
-            type={"text"}
-            label={"Last name"}
-            variant={"outlined"}
-            fullWidth={"true"}
-          />
-          <TextField
-            type={"email"}
-            label={"Email"}
-            variant={"outlined"}
-            fullWidth={"true"}
-          />
-          <TextField
-            type={"number"}
-            label={"Age"}
-            variant={"outlined"}
-            fullWidth={"true"}
-          />
-          <TextField
-            type={"tel"}
-            label={"Phone number"}
-            variant={"outlined"}
-            fullWidth={"true"}
-          />
-          <FormControlLabel value="female" control={<Radio />} label="Female" />
-          <FormControlLabel value="male" control={<Radio />} label="Male" />
-          <FormControlLabel value="other" control={<Radio />} label="Other" />
-          <TextField
-            type={"text"}
-            label={"Role"}
-            variant={"outlined"}
-            fullWidth={"true"}
-          />
-          <TextField
-            type={"image"}
-            label={"Upload image"}
-            variant={"outlined"}
-            fullWidth={"true"}
-          />
+        <form onSubmit>
+          <FormControl
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              gap: 2,
+              flexWrap: "wrap",
+              justifyContent: "center",
+            }}
+            fullWidth={true}
+          >
+            <TextField
+              type={"text"}
+              label={"First name"}
+              variant={"outlined"}
+              fullWidth={true}
+            />
+            <TextField
+              type={"text"}
+              label={"Last name"}
+              variant={"outlined"}
+              fullWidth={true}
+            />
+            <TextField
+              type={"email"}
+              label={"Email"}
+              variant={"outlined"}
+              fullWidth={true}
+            />
+            <TextField
+              type={"number"}
+              label={"Age"}
+              variant={"outlined"}
+              fullWidth={true}
+            />
+            <TextField
+              type={"tel"}
+              label={"Phone number"}
+              variant={"outlined"}
+              fullWidth={true}
+            />
+            <FormControlLabel
+              value="female"
+              control={<Radio />}
+              label="Female"
+            />
+            <FormControlLabel value="male" control={<Radio />} label="Male" />
+            <FormControlLabel value="other" control={<Radio />} label="Other" />
+            <TextField
+              type={"text"}
+              label={"Role"}
+              variant={"outlined"}
+              fullWidth={true}
+            />
+            <TextField
+              type={"file"}
+              label={"Upload image"}
+              variant={"outlined"}
+              fullWidth={true}
+            />
 
-          <TextField
-            type={"password"}
-            label={"Password"}
-            variant={"outlined"}
-            fullWidth={"true"}
-          />
+            <TextField
+              type={"password"}
+              label={"Password"}
+              variant={"outlined"}
+              fullWidth={true}
+            />
+          </FormControl>
 
-          <Button variant={"outlined"} color={"primary"}>
+          <Button variant={"outlined"} type={"submit"} color={"primary"}>
             Submit
           </Button>
-        </FormControl>
+        </form>
       </Box>
     </Container>
   );
